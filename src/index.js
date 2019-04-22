d8755b7cc8c63f568287951d0f33d1d005b5c71c
paige0701 <paigechoi0701@gmail.com>
2019-04-22 22:38:38 +0900

    add some comments

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// function component 는 그냥 render method 만 있고 자기 state를 관리 하지 않는 것을 말한다.
+// 상위 컴포넌트에서 스테이트를 관리 하기 때문에 square에서는 그냥 값만 보여주는 역할을 하는것 같다.
+// 파라미터로 props를 받아 올 수 있다1
 function Square(props) {
     return (
         <button className="square" onClick={props.onClick}>
@@ -69,10 +72,15 @@ class Board extends React.Component {
     }
 }
 
+// 게임이 모든 클라스를 갖고 있다.
 class Game extends React.Component {
 
     constructor(props) {
+        // 생성자를 갖고 있는 React 클라스 꼭 super(props); 를 먼저 선언한다
         super(props);
+
+        // 컴포넌트가 무언가 기억해야 할 때 this.state 를 사용한다.
+        // 생성자 안에 넣어서 state 를 초기화 한다.
         this.state = {
             history : [{
                 squares: Array(9).fill(null),
@@ -130,6 +138,11 @@ class Game extends React.Component {
         } else {
             status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
+
+        // Parent component 에서 children 의 state 를 관리 하는것이 좋다고 tutorial 에는 써있다.
+        // Parent 컴포넌트에서 child 컴포넌트로 값을 넘길때는 props 를 사용한다.
+        // 아래 <Board 태그 안에 선언 되어 있는 squares 와 onClick 이 있는데
+        // child 컴포넌트에서 this.props.squares 나 this.props.onClick 으로 값을 받아올 수 있다,
         return (
             <div className="game">
                 <div className="game-board">
